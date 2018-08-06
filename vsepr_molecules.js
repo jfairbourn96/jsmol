b3e0ed195d5d636e4a4cd2913de76f8d41b2acca
Justin Fairbourn <jfairbourn96@bitbucket.org>
2018-08-06 16:05:09 -0600

    - Separated molecules.js into vsepr_molecules.js and  carbon_molecules.js  - Refactored carbon_allotropes.html    - moved the code within the anonmymous window load event function    into an identified function: initWindow(), and reorganized the code    within    - added new comments as necessary  - Refactored vsepr_configurations.html    - modified the anonymous function same as in carbon_allotropes.html    - added comments as necessary    - added a dynamic <div> that displays the formula of the molecule    being viewed.  - Added styling within the molecule-pages.css for the changes in  vsepr_configurations.html

--- vsepr_molecules.js
+++ vsepr_molecules.js
@@ -1,3 +1,6 @@
+//====================================================
+//= 14 molecules loaded in vsepr_configurations.html =
+//====================================================
 var vsepr_01 = {
 	e_domains: 2, 
 	bonded: 2, 
@@ -5,7 +8,7 @@ var vsepr_01 = {
 	e_geometry: "linear", 
 	m_geometry: "linear", 
 	path: "PDB/vsepr_01_BeCl2.mol",
-	formula: "BeCl2",
+	formula: "BeCl<sub>2</sub>",
 	lone_pair_command: ""
 };
 
@@ -16,7 +19,7 @@ var vsepr_02 = {
 	e_geometry: "linear",
 	m_geometry: "linear",
 	path: "PDB/vsepr_02_N2.mol",
-	formula: "N2",
+	formula: "N<sub>2</sub>",
 	lone_pair_command: "select *; lcaoCartoon ON; lcaoCartoon SCALE 2 COLOR TRANSLUCENT [31,240,31] CREATE spb;"
 };
 
@@ -27,7 +30,7 @@ var vsepr_03 = {
 	e_geometry: "trigonal planar",
 	m_geometry: "trigonal planar",
 	path: "PDB/vsepr_03_BF3.mol",
-	formula: "BF3",
+	formula: "BF<sub>3</sub>",
 	lone_pair_command: ""
 };
 
@@ -38,7 +41,7 @@ var vsepr_04 = {
 	e_geometry: "trigonal planar",
 	m_geometry: "bent",
 	path: "PDB/vsepr_04_O3.mol",
-	formula: "O3",
+	formula: "O<sub>3</sub>",
 	lone_pair_command: "select atomno=1; lcaoCartoon SCALE 2 COLOR TRANSLUCENT [31,240,31] CREATE sp2c;"
 };
 
@@ -49,7 +52,7 @@ var vsepr_05 = {
 	e_geometry: "tetrahedral",
 	m_geometry: "tetrahedral",
 	path: "PDB/vsepr_05_CH4.mol",
-	formula: "CH4",
+	formula: "CH<sub>4</sub>",
 	lone_pair_command: ""
 };
 
@@ -60,7 +63,7 @@ var vsepr_06 = {
 	e_geometry: "tetrahedral",
 	m_geometry: "trigonal pyramidal",
 	path: "PDB/vsepr_06_H3O.mol",
-	formula: "H3O",
+	formula: "H<sub>3</sub>O",
 	lone_pair_command: "select atomno=1; lcaoCartoon SCALE 1.5 COLOR TRANSLUCENT [31,240,31] CREATE sp3d;"
 };
 
@@ -71,7 +74,7 @@ var vsepr_07 = {
 	e_geometry: "tetrahedral",
 	m_geometry: "bent",
 	path: "PDB/vsepr_07_H2O.mol",
-	formula: "H2O",
+	formula: "H<sub>2</sub>O",
 	lone_pair_command: "select atomno=1; lcaoCartoon SCALE 1.3 COLOR TRANSLUCENT [31,240,31] CREATE sp3c; lcaoCartoon CREATE sp3d;"
 	
 };
@@ -83,7 +86,7 @@ var vsepr_08 = {
 	e_geometry: "trigonal bipyramidal",
 	m_geometry: "trigonal bipyramidal",
 	path: "PDB/vsepr_08_PCl5.mol",
-	formula: "PCl5",
+	formula: "PCl<sub>5</sub>",
 	lone_pair_command: ""
 };
 
@@ -94,7 +97,7 @@ var vsepr_09 = {
 	e_geometry: "trigonal bipyramidal",
 	m_geometry: "see saw",
 	path: "PDB/vsepr_09_SF4.pdb",
-	formula: "SF4",
+	formula: "SF<sub>4</sub>",
 	lone_pair_command: "select atomno=1; lcaoCartoon SCALE 2.5 COLOR TRANSLUCENT [31,240,31] CREATE sp3de;"
 };
 
@@ -105,7 +108,7 @@ var vsepr_10 = {
 	e_geometry: "trigonal bipyramidal",
 	m_geometry: "t-shaped",
 	path: "PDB/vsepr_10_ClF3.mol",
-	formula: "ClF3",
+	formula: "ClF<sub>3</sub>",
 	lone_pair_command: "select atomno=1; lcaoCartoon SCALE 2 COLOR TRANSLUCENT [31,240,31] CREATE sp3de; lcaoCartoon CREATE sp3dd;"
 };
 
@@ -116,7 +119,7 @@ var vsepr_11 = {
 	e_geometry: "trigonal bipyramidal",
 	m_geometry: "t-shaped",
 	path: "PDB/vsepr_11_XeF2.mol",
-	formula: "XeF2"
+	formula: "XeF<sub>2</sub>"
 };
 
 var vsepr_12 = {
@@ -126,7 +129,7 @@ var vsepr_12 = {
 	e_geometry: "octahedral",
 	m_geometry: "octahedral",
 	path: "PDB/vsepr_12_SF6.mol",
-	formula: "SF6",
+	formula: "SF<sub>6</sub>",
 	lone_pair_command: ""
 };
 
@@ -137,7 +140,7 @@ var vsepr_13 = {
 	e_geometry: "octahedral",
 	m_geometry: "square pyramidal",
 	path: "PDB/vsepr_13_BrF5.mol",
-	formula: "BrF5",
+	formula: "BrF<sub>5</sub>",
 	lone_pair_command: "lcaoCartoon SCALE 2.5 COLOR TRANSLUCENT [31,240,31] CREATE sp3d2f;"
 };
 
@@ -148,33 +151,37 @@ var vsepr_14 = {
 	e_geometry: "octahedral",
 	m_geometry: "square planar",
 	path: "PDB/vsepr_14_ICl4.xyz",
-	formula: "ICl4",
+	formula: "ICl<sub>4</sub>",
 	lone_pair_command: "select atomno=1; lcaoCartoon SCALE 2.5 COLOR TRANSLUCENT [31,240,31] CREATE sp3d2f; lcaoCartoon CREATE sp3d2e;"
 };
+
+//================================================
+//= 6 molecules loaded in carbon_allotropes.html =
+//================================================
 
 var buckyball = {
 	title: "Buckyball",
 	path: "PDB/buckyball.pdb",
 	description: "<h4>Structure of C60, buckminsterfullerene (buckyball)</h4> \
-	<p>Buckminster fullerene is an allotrope of carbon containing exactly 60 C atoms bonded together in a cage-like ring structure. The C atoms are bonded in such a fashion that they form 20 hexagonal and 12 pentagonal interconnected units.</p> \
-	<p>Each C atom is sp<sup>2</sup>-hybridized and contains 3 sigma bonds and one pi bond, where the pi (double) bonds may be represented as shown in the figure below. In reality, the pi bonds are resonance-delocalized over the entire molecule.</p> \
-	<p>Each hexagonal and pentagonal unit is planar. Since the ring atoms are wrapped into a spherical structure, some bond angles for the C atoms must be compressed below the optimal 120 degrees for sp<sup>2</sup>-hybridized C atoms, inducing angle strain.</p> \
-	<img src=\"jpegs/Buckminsterfullerene.svg.png\"> "
+	<p>Buckminster fullerene is an allotrope of carbon containing exactly 60 C atoms bonded together in a cage-like ring structure. The C atoms are bonded in such a fashion that they form 20 hexagonal and 12 pentagonal interconnected units.</p>" +
+	"<p>Each C atom is sp<sup>2</sup>-hybridized and contains 3 sigma bonds and one pi bond, where the pi (double) bonds may be represented as shown in the figure below. In reality, the pi bonds are resonance-delocalized over the entire molecule.</p>" +
+	"<p>Each hexagonal and pentagonal unit is planar. Since the ring atoms are wrapped into a spherical structure, some bond angles for the C atoms must be compressed below the optimal 120 degrees for sp<sup>2</sup>-hybridized C atoms, inducing angle strain.</p>" +
+	"<img src=\"jpegs/Buckminsterfullerene.svg.png\"> "
 };
 
 var diamond = {
 	title: "Diamond",
 	path: "PDB/diamond.pdb",
-	description: "<h4>Structure of diamond</h4> \
-	<p>Diamond is a covalent network allotrope of carbon where each C atom is sp<sup>3</sup>-hybridized and is bonded to 4 additional C atoms in a large 3-dimensional lattice. All of the bond angles in the lattice are 109.5 degrees. Diamond is formed from the rearrangement of chemical bonds when the standard state form of carbon (graphite) is subjected to high pressures. Like graphite and graphene, diamond must be represented by an empirical formula (C) since there is not a defined number of atoms. </p>"
+	description: "<h4>Structure of diamond</h4>" +
+	"<p>Diamond is a covalent network allotrope of carbon where each C atom is sp<sup>3</sup>-hybridized and is bonded to 4 additional C atoms in a large 3-dimensional lattice. All of the bond angles in the lattice are 109.5 degrees. Diamond is formed from the rearrangement of chemical bonds when the standard state form of carbon (graphite) is subjected to high pressures. Like graphite and graphene, diamond must be represented by an empirical formula (C) since there is not a defined number of atoms. </p>"
 };
 
 var graphite = {
 	title: "Graphite",
 	path: "PDB/graphite.pdb",
-	description: "<h4>Structure of graphite</h4> \
-	<p>Graphite is the standard state form of the element carbon, meaning it is the form most stable at 25 degrees C and normal atmospheric pressure. Graphite is a covalent network of carbon atoms, where each individual carbon atom is bonded to 3 additional carbon atoms. The orbital hybridization of the C atoms is sp<sup>2</sup>, resulting in 120 degree bond angles and trigonal planar geometry. All of the bonded C atoms lie in a single plane (layer, or sheet) which contains repeating 6-membered rings of C atoms (a hexagonal lattice). Each C atom contains a single electron in a nonhybridized p orbital that is delocalised over the ring systems of the layer. Individual layers of graphite, called graphene, stack on top of each other to form the 3-dimensional structure shown in the model at right. Only a small portion of each sheet is shown in this model; in reality the individual sheets will contain a much larger number of C atoms. There is no true molecular formula for graphite as the number of carbon atoms in layers is not a fixed number, and it is accordingly represented by the empirical formula (C). </p> \
-	<p>Graphite is an electrical conductor due to the presence of the electrons in the nonhybridized p orbitals. </p>"
+	description: "<h4>Structure of graphite</h4>" +
+	"<p>Graphite is the standard state form of the element carbon, meaning it is the form most stable at 25 degrees C and normal atmospheric pressure. Graphite is a covalent network of carbon atoms, where each individual carbon atom is bonded to 3 additional carbon atoms. The orbital hybridization of the C atoms is sp<sup>2</sup>, resulting in 120 degree bond angles and trigonal planar geometry. All of the bonded C atoms lie in a single plane (layer, or sheet) which contains repeating 6-membered rings of C atoms (a hexagonal lattice). Each C atom contains a single electron in a nonhybridized p orbital that is delocalised over the ring systems of the layer. Individual layers of graphite, called graphene, stack on top of each other to form the 3-dimensional structure shown in the model at right. Only a small portion of each sheet is shown in this model; in reality the individual sheets will contain a much larger number of C atoms. There is no true molecular formula for graphite as the number of carbon atoms in layers is not a fixed number, and it is accordingly represented by the empirical formula (C). </p>" +
+	"<p>Graphite is an electrical conductor due to the presence of the electrons in the nonhybridized p orbitals. </p>"
 };
 
 var graphene_1 = {
