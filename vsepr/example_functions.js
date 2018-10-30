318125afe0fee2470b9f575bc8aec0ff1f42d20c
Justin Fairbourn <jfairbourn96@bitbucket.org>
2018-10-29 18:02:53 -0600

    vsepr_examples.html correctly loads the molecules. Now I need to add add the descriptions and the formulas and the titles to the molecules so it changes the html on new molecule load.

--- vsepr/example_functions.js
+++ vsepr/example_functions.js
@@ -9,7 +9,7 @@ let info = {
   width: 500,
   j2sPath: "../jsmol/j2s",
   // Change the initial molecule here.
-  script: "load ../PDB/vsepr_01_BeCl2.mol; spacefill 20%; wireframe 0.15; spin off; frank on; background=white; antialiasDisplay=true; set showMultipleBonds OFF;",
+  script: "load ../PDB/BeH2.mol; spacefill 20%; wireframe 0.15; spin off; frank on; background=white; antialiasDisplay=true; set showMultipleBonds OFF;",
   debug: false
 };
 
@@ -24,81 +24,100 @@ function initWindow(){
   var moleculeSelect = document.getElementById("configuration");
   
   // initialize all 14 options within the select
-  var option01 = document.createElement("option");
-  var option02 = document.createElement("option");
-  var option03 = document.createElement("option");
-  var option04 = document.createElement("option");
-  var option05 = document.createElement("option");
-  var option06 = document.createElement("option");
-  var option07 = document.createElement("option");
-  var option08 = document.createElement("option");
-  var option09 = document.createElement("option");
-  var option10 = document.createElement("option");
-  var option11 = document.createElement("option");
-  var option12 = document.createElement("option");
-  var option13 = document.createElement("option");
-  var option14 = document.createElement("option");
+  let options = {
+    option_01: document.createElement("option"),
+    option_02: document.createElement("option"),
+    option_03: document.createElement("option"),
+    option_04: document.createElement("option"),
+    option_05: document.createElement("option"),
+    option_06: document.createElement("option"),
+    option_07: document.createElement("option"),
+    option_08: document.createElement("option"),
+    option_09: document.createElement("option"),
+    option_10: document.createElement("option"),
+    option_11: document.createElement("option"),
+    option_12: document.createElement("option"),
+    option_13: document.createElement("option"),
+    option_14: document.createElement("option"),
+    option_15: document.createElement("option"),
+  }
   
   // assign each option a corresponding molecule
-  option01.molecule = vsepr_01;
-  option02.molecule = vsepr_02;
-  option03.molecule = vsepr_03;
-  option04.molecule = vsepr_04;
-  option05.molecule = vsepr_05;
-  option06.molecule = vsepr_06;
-  option07.molecule = vsepr_07;
-  option08.molecule = vsepr_08;
-  option09.molecule = vsepr_09;
-  option10.molecule = vsepr_10;
-  option11.molecule = vsepr_11;
-  option12.molecule = vsepr_12;
-  option13.molecule = vsepr_13;
-  option14.molecule = vsepr_14;
+  options.option_01.molecule = molecules.BeH2;
+  options.option_02.molecule = molecules.CO2;
+  options.option_03.molecule = molecules.N2;
+  options.option_04.molecule = molecules.BH3;
+  options.option_05.molecule = molecules.SO2;
+  options.option_06.molecule = molecules.CH4;
+  options.option_07.molecule = molecules.NH3;
+  options.option_08.molecule = molecules.H2O;
+  options.option_09.molecule = molecules.PF5;
+  options.option_10.molecule = molecules.SF4;
+  options.option_11.molecule = molecules.ClF3;
+  options.option_12.molecule = molecules.XeF2;
+  options.option_13.molecule = molecules.SF6;
+  options.option_14.molecule = molecules.IF5;
+  options.option_15.molecule = molecules.XeF4;
   
   // give each option an appropriate label
-  option01.label = "Linear";
-  option02.label = "Linear (2)";
-  option03.label = "Trigonal Planar";
-  option04.label = "Trigonal Planar - Bent";
-  option05.label = "Tetrahedral";
-  option06.label = "Tetrahedral - Trigonal Pyramidal";
-  option07.label = "Tetrahedral - Bent";
-  option08.label = "Trigonal Bipyramidal";
-  option09.label = "Trigonal Bipyramidal - Seesaw";
-  option10.label = "Trigonal Bipyramidal - T-shaped";
-  option11.label = "Trigonal Bipyramidal - Linear";
-  option12.label = "Octahedral";
-  option13.label = "Octahedral - Square Pyramidal";
-  option14.label = "Octahedral - Square Planar";
+  options.option_01.label = "BeH2";
+  options.option_02.label = "CO2";
+  options.option_03.label = "N2";
+  options.option_04.label = "BH3";
+  options.option_05.label = "SO2";
+  options.option_06.label = "CH4";
+  options.option_07.label = "NH3";
+  options.option_08.label = "H2O";
+  options.option_09.label = "PF5";
+  options.option_10.label = "SF4";
+  options.option_11.label = "ClF3";
+  options.option_12.label = "XeF2";
+  options.option_13.label = "SF6";
+  options.option_14.label = "IF5";
+  options.option_15.label = "XeF4";
   
   // link each option to the <select> element
-  moleculeSelect.add(option01);
-  moleculeSelect.add(option02);
-  moleculeSelect.add(option03);
-  moleculeSelect.add(option04);
-  moleculeSelect.add(option05);
-  moleculeSelect.add(option06);
-  moleculeSelect.add(option07);
-  moleculeSelect.add(option08);
-  moleculeSelect.add(option09);
-  moleculeSelect.add(option10);
-  moleculeSelect.add(option11);
-  moleculeSelect.add(option12);
-  moleculeSelect.add(option13);
-  moleculeSelect.add(option14);
+  moleculeSelect.add(options.option_01);
+  moleculeSelect.add(options.option_02);
+  moleculeSelect.add(options.option_03);
+  moleculeSelect.add(options.option_04);
+  moleculeSelect.add(options.option_05);
+  moleculeSelect.add(options.option_06);
+  moleculeSelect.add(options.option_07);
+  moleculeSelect.add(options.option_08);
+  moleculeSelect.add(options.option_09);
+  moleculeSelect.add(options.option_10);
+  moleculeSelect.add(options.option_11);
+  moleculeSelect.add(options.option_12);
+  moleculeSelect.add(options.option_13);
+  moleculeSelect.add(options.option_14);
+  moleculeSelect.add(options.option_15);
   
   // IMPORTANT: Set an option as the default selected choice
-  option01.selected = "selected";
+  let urlArg = getURLMol();
+
+  if (urlArg in options) {
+    options[urlArg].selected = "selected";
+  } else {
+    options.option_01.selected = "selected";
+  }
   
   loadNew();
 };
+
+// Function that checks the url for a mol option argument
+function getURLMol() {
+  return decodeURI(
+    (new RegExp('mol=' + '(.+?)(&|$)').exec(location.search) || [null])[1]
+  );
+}
 
 // Resets defaults and loads the appropriate molecule.
 function loadNew() {
   // load the appropriate molecule
   let molecule = getCurrentMolecule();
   let loadString = `load  ${molecule.path}; select *; spacefill 0.4; `;
-  let colorString = `select *; color purple; select atomno=${molecule.center_atomno}; color white;`
+  // let colorString = `select *; color purple; select atomno=${molecule.center_atomno}; color white;`
 
   // Hide the lone pair checkbox if there are no lone pairs
   if (molecule.nonbonded === 0) {
@@ -114,9 +133,10 @@ function loadNew() {
   document.getElementById("co_spin").checked = false;
   document.getElementById("co_lonePair").checked = false;
   
-  Jmol.script(main, loadString+colorString);
+  Jmol.script(main, loadString);
+  // Jmol.script(main, loadString+colorString);
 
-  if(molecule === vsepr_11) Jmol.script(main, "hide atomno=4;");
+  if(molecule === molecules.XeF2) Jmol.script(main, "hide atomno=4;");
 }
 
 function getCurrentMolecule(){
--- vsepr/example_molecules.js
+++ vsepr/example_molecules.js
@@ -1,3 +1,5 @@
+// Authors: Justin Fairbourn and Scott Ensign
+// Created at Utah State University with support from the Chemistry department and the Center for Innovative Design and Instruction.
 /* MOLECULES
 Mol   pdb 
 BeH2  +    
@@ -29,28 +31,168 @@ lone_pair_command: "select *; lcaoCartoon ON; lcaoCartoon SCALE 2 COLOR TRANSLUC
 
 let molecules = {
   BeH2: {
-    e_domains: 0,
-    bonded: 0,
+    e_domains: 2,
+    bonded: 2,
     nonbonded: 0,
-    e_geometry: "",
-    m_geometry: "",
-    path: "",
+    e_geometry: "linear",
+    m_geometry: "linear",
+    path: "../PDB/BeH2.mol",
     formula_html: "",
     description_html: "",
     lone_pair_command: ""
   },
-  CO2: {},
-  N2: {},
-  BH3: {},
-  SO2: {},
-  CH4: {},
-  NH3: {},
-  H2O: {},
-  PF5: {},
-  SF4: {},
-  ClF3: {},
-  XeF2: {},
-  SF6: {},
-  IF5: {},
-  XeF4: {},
+  CO2: {
+    e_domains: 2,
+    bonded: 2,
+    nonbonded: 0,
+    e_geometry: "linear",
+    m_geometry: "linear",
+    path: "../PDB/CO2.mol",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  N2: {
+    e_domains: 2,
+    bonded: 1,
+    nonbonded: 1,
+    e_geometry: "linear",
+    m_geometry: "linear",
+    path: "../PDB/vsepr_02_N2.mol",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  BH3: {
+    e_domains: 3,
+    bonded: 3,
+    nonbonded: 0,
+    e_geometry: "trigonal planar",
+    m_geometry: "trigonal planar",
+    path: "../PDB/BH3.mol",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  SO2: {
+    e_domains: 3,
+    bonded: 2,
+    nonbonded: 1,
+    e_geometry: "trigonal planar",
+    m_geometry: "bent",
+    path: "../PDB/SO2.mol",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  CH4: {
+    e_domains: 4,
+    bonded: 4,
+    nonbonded: 0,
+    e_geometry: "tetrahedral",
+    m_geometry: "tetrahedral",
+    path: "../PDB/vsepr_05_CH4.mol",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  NH3: {
+    e_domains: 4,
+    bonded: 3,
+    nonbonded: 1,
+    e_geometry: "tetrahedral",
+    m_geometry: "trigonal pyramidal",
+    path: "../PDB/NH3opt.xyz",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  H2O: {
+    e_domains: 4,
+    bonded: 2,
+    nonbonded: 2,
+    e_geometry: "tetrahedral",
+    m_geometry: "bent",
+    path: "../PDB/vsepr_07_H2O.mol",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  PF5: {
+    e_domains: 5,
+    bonded: 5,
+    nonbonded: 0,
+    e_geometry: "trigonal bipyramidal",
+    m_geometry: "trigonal bipyramidal",
+    path: "../PDB/PF5.mol",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  SF4: {
+    e_domains: 5,
+    bonded: 4,
+    nonbonded: 1,
+    e_geometry: "trigonal bipyramidal",
+    m_geometry: "see saw",
+    path: "../PDB/vsepr_09_SF4.pdb",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  ClF3: {
+    e_domains: 5,
+    bonded: 3,
+    nonbonded: 2,
+    e_geometry: "trigonal bipyramidal",
+    m_geometry: "T shaped",
+    path: "../PDB/vsepr_10_ClF3.mol",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  XeF2: {    
+    e_domains: 5,
+    bonded: 2,
+    nonbonded: 3,
+    e_geometry: "trigonal bipyramidal",
+    m_geometry: "linear",
+    path: "../PDB/vsepr_10_ClF3.mol",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  SF6: {
+    e_domains: 6,
+    bonded: 6,
+    nonbonded: 0,
+    e_geometry: "octahedral",
+    m_geometry: "octahedral",
+    path: "../PDB/vsepr_12_SF6.mol",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  IF5: {
+    e_domains: 6,
+    bonded: 5,
+    nonbonded: 1,
+    e_geometry: "octahedral",
+    m_geometry: "square pyramidal",
+    path: "../PDB/IF5.mol",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
+  XeF4: {
+    e_domains: 6,
+    bonded: 4,
+    nonbonded: 2,
+    e_geometry: "octahedral",
+    m_geometry: "square planar",
+    path: "../PDB/vsepr_14_ICl4.xyz",
+    formula_html: "",
+    description_html: "",
+    lone_pair_command: ""
+  },
 }
