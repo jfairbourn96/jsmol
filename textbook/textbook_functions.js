c1f4f09661f35819599c484169a0bcfed7ee78b8
Justin Fairbourn <jfairbourn96@bitbucket.org>
2018-11-15 16:53:02 -0700

    Added mol files for the textbook page, finished the molecules and function js complete with isosurface/partialcharge functionality.

--- textbook/textbook_functions.js
+++ textbook/textbook_functions.js
@@ -30,25 +30,45 @@ function initWindow(){
     option_02: document.createElement("option"),
     option_03: document.createElement("option"),
     option_04: document.createElement("option"),
+    option_05: document.createElement("option"),
+    option_06: document.createElement("option"),
+    option_07: document.createElement("option"),
+    option_08: document.createElement("option"),
+    option_09: document.createElement("option"),
   }
 
   // assign each option a corresponding molecule
-  options.option_01.molecule = molecules.ethylene;
-  options.option_02.molecule = molecules.acetylene;
-  options.option_03.molecule = molecules.carbon_monoxide;
-  options.option_04.molecule = molecules.formaldehyde;
+  options.option_01.molecule = molecules.water;
+  options.option_02.molecule = molecules.ammonia;
+  options.option_03.molecule = molecules.salt_lattice;
+  options.option_04.molecule = molecules.acetic_acid;
+  options.option_05.molecule = molecules.sulfuric_acid;
+  options.option_06.molecule = molecules.ethanol;
+  options.option_07.molecule = molecules.dimethyl_ether;
+  options.option_08.molecule = molecules.hydrogen_sulfate;
+  options.option_09.molecule = molecules.sulfate;
 
   // give each option an appropriate label
   options.option_01.innerHTML = options.option_01.molecule.name;
   options.option_02.innerHTML = options.option_02.molecule.name;
   options.option_03.innerHTML = options.option_03.molecule.name;
   options.option_04.innerHTML = options.option_04.molecule.name;
+  options.option_05.innerHTML = options.option_05.molecule.name;
+  options.option_06.innerHTML = options.option_06.molecule.name;
+  options.option_07.innerHTML = options.option_07.molecule.name;
+  options.option_08.innerHTML = options.option_08.molecule.name;
+  options.option_09.innerHTML = options.option_09.molecule.name;
   
   // link each option to the <select> element
   moleculeSelect.add(options.option_01);
   moleculeSelect.add(options.option_02);
   moleculeSelect.add(options.option_03);
   moleculeSelect.add(options.option_04);
+  moleculeSelect.add(options.option_05);
+  moleculeSelect.add(options.option_06);
+  moleculeSelect.add(options.option_07);
+  moleculeSelect.add(options.option_08);
+  moleculeSelect.add(options.option_09);
 
   // IMPORTANT: Set an option as the default selected choice
   let urlArg = getURLMol();
@@ -80,13 +100,27 @@ function loadNew() {
   document.getElementById("name").innerHTML = molecule.name;
   document.getElementById("mt_ballstick").checked = true;
   document.getElementById("co_spin").checked = false;
+  document.getElementById("isosurface").checked = false;
+  document.getElementById("partial_charge").checked = false;
   
+  if(molecule.name === "Water" || molecule.name === "NaCl Lattice"){
+    getBox("isosurface").disabled = true;
+    getBox("partial_charge").disabled = true;
+  } else {
+    getBox("isosurface").disabled = false;
+    getBox("partial_charge").disabled = false;
+  }
+
   Jmol.script(main, loadString);
 }
 
 function getCurrentMolecule(){
   moleculeSelect = document.getElementById("configuration");
   return moleculeSelect.options[moleculeSelect.selectedIndex].molecule;
+}
+
+function getBox(id) {
+  return document.getElementById(id);
 }
 
 function toggleSpin() {
@@ -112,4 +146,24 @@ function stick() {
 function changeColor(color) {
   var colorString = 'background ' + color + ';';
   Jmol.script(main, colorString);
+}
+
+function toggleIsosurface() {
+  getBox('partial_charge').checked = false;
+  if (getBox('isosurface').checked) {
+    Jmol.script(main, 'isosurface off');
+    Jmol.script(main, 'lcaoCartoon off');
+    Jmol.script(main, 'isosurface vdw; isosurface translucent;');
+  } else {
+    Jmol.script(main, 'select *; isosurface off;');
+  }
+}
+
+function togglePartialCharges() {
+  Jmol.script(main, 'isosurface off');
+  Jmol.script(main, 'lcaoCartoon off');
+  getBox('isosurface').checked = false;
+  if (getBox('partial_charge').checked) {
+    Jmol.script(main, 'isosurface off; if ({atomno < 20}.partialcharge == 0){calculate partialcharge}; isosurface vdw map mep; isosurface translucent;')
+  }
 }
--- textbook/textbook_molecules.js
+++ textbook/textbook_molecules.js
@@ -1,22 +1,47 @@
 let molecules = {
-  ethylene: {
-    name: "Ethylene",
-    formula: "C<sub>2</sub>H<sub>4</sub>",
-    path: `../PDB/ethene.mol`,
+  water: {
+    name: "Water",
+    formula: "H<sub>2</sub>O",
+    path: "../PDB/h2o-liquid.pdb",
   },
-  acetylene: {
-    name: "Acetylene",
-    formula: "C<sub>2</sub>H<sub>2</sub>",
-    path: `../PDB/ethyne.mol`
+  ammonia: {
+    name: "Ammonia",
+    formula: "NH<sub>3</sub>",
+    path: "../PDB/NH3opt.xyz", 
   },
-  carbon_monoxide: {
-    name: "Carbon Monoxide",
-    formula: "CO",
-    path: `../PDB/carbon_monoxide.mol`
+  salt_lattice: {
+    name: "NaCl Lattice",
+    formula: "NaCl",
+    path: "../PDB/NaCl.pdb",
   },
-  formaldehyde: {
-    name: "Formaldehyde",
-    formula: "CH<sub>2</sub>O",
-    path: `../PDB/formaldehyde.mol`
+  acetic_acid: {
+    name: "Acetic Acid",
+    formula: "CH<sub>3</sub>COOH",
+    path: "../PDB/acetic_acid.mol",
+  },
+  sulfuric_acid: {
+    name: "Sulfuric Acid",
+    formula: "H<sub>2</sub>SO4",
+    path: "../PDB/H2SO4.mol",
+  },
+  ethanol: {
+    name: "Ethanol",
+    formula: "C<sub>2</sub>H<sub>5</sub>OH",
+    path: "../PDB/ethanol.mol",
+  },
+  dimethyl_ether: {
+    name: "Dimethyl Ether",
+    formula: "C<sub>2</sub>H<sub>6</sub>O",
+    path: "../PDB/dimethylether.pdb",
+  },
+  hydrogen_sulfate: {
+    name: "Hydrogen Sulfate Anion",
+    formula: "HSO4<sup>-</sup>",
+    path: "../PDB/hydrogen_sulfate.mol",
+  },  
+  sulfate: {
+    name: "Sulfate Anion",
+    formula: "SO4<sup>-2</sup>",
+    path: "../PDB/sulfate.mol",
   },
 }
